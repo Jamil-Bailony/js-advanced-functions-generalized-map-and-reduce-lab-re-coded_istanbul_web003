913e032749adbc3a631d56b9b4fea04f16ff1a7f
Jamil Bailony <jBailony@outlook.com>
2020-08-23 17:20:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your functions here
+function map(array, callback) {
+    let r = []
+
+    for (let i = 0; i < array.length; i++) {
+        r.push(callback(array[i]))
+    }
+
+    return r
+}
+
+function reduce(array, callback, startingValue = 'notGiven') {
+    let total, start;
+    if(startingValue !== 'notGiven') {
+        total = startingValue;
+        start = 0;
+    } else {
+        total = array[0];
+        start = 1;
+    }
+
+    for (let i = start; i < array.length; i++) {
+        console.log(total);
+        total = callback(total, array[i]);
+    }
+
+    return total
+}
